ddb1a1c99a63eb86c1206afe786b2470116ff28f
Tuan Ngo Thanh <tuan.nt@hoiana.com>
2024-05-06 11:39:56 +0700

    feat: Update location

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ import {
   Select,
   SkeletonText,
   Text,
-} from '@chakra-ui/react'
-import { FaLocationArrow, FaTimes } from 'react-icons/fa'
+} from "@chakra-ui/react";
+import { FaLocationArrow, FaTimes } from "react-icons/fa";
 
 import {
   useJsApiLoader,
@@ -18,99 +18,113 @@ import {
   Marker,
   Autocomplete,
   DirectionsRenderer,
-} from '@react-google-maps/api'
-import { useEffect, useRef, useState } from 'react'
-import { getHistory, getRealtime } from './services/api'
+} from "@react-google-maps/api";
+import { useEffect, useRef, useState } from "react";
+import { getHistory, getRealtime } from "./services/api";
 
+// const center = { lat: 15.83226, lng: 108.40632 }; // Default location when start app
 const center = { lat: 16.05435, lng: 108.20848 } // Get lat and lang from realtime API
 
 function App() {
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: 'AIzaSyDqmGRp_FcaQyKKZI4jF6jTtqbRLOAnM38', // Config to env
-    libraries: ['places'],
-  })
+    googleMapsApiKey: "AIzaSyDqmGRp_FcaQyKKZI4jF6jTtqbRLOAnM38", // Config to env
+    libraries: ["places"],
+  });
 
-  const [map, setMap] = useState(/** @type google.maps.Map */ (null))
-  const [directionsResponse, setDirectionsResponse] = useState(null)
-  const [distance, setDistance] = useState('')
-  const [duration, setDuration] = useState('')
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null));
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
 
   /** @type React.MutableRefObject<HTMLInputElement> */
-  const originRef = useRef()
+  const originRef = useRef();
   /** @type React.MutableRefObject<HTMLInputElement> */
-  const destiantionRef = useRef()
-
-  // Call api history
-  useEffect(() => {
-    const fetchHistories = async () => {
-      const data = await getHistory()
-      console.log('log2', data.result.routes[0])
-      setHistories(data)
-    }
-
-    fetchHistories()
-  }, [])
-
-  // Call api realtime 
-  useEffect(() => { // TODO: try catch error
-    const fetchRealtime = async () => {
-      console.log('realtime')
-      const data = await getRealtime()
-      console.log("logggg2", data)
-      setRealtime(data)
-    }
-
-    fetchRealtime()
-  }, [])
-
-  if (!isLoaded) {
-    return <SkeletonText />
-  }
+  const destiantionRef = useRef();
+  const [realtime, setRealtime] = useState(null);
 
   // Prepare bus stops - Fake datas
   const busStops = [
     {
       location:
-        'Bệnh Viện Phụ Sản - Nhi Đà Nẵng, Đường Lê Văn Hiến, Khuê Mỹ, Ngũ Hành Sơn, Da Nang, Vietnam',
+        "Bệnh Viện Phụ Sản - Nhi Đà Nẵng, Đường Lê Văn Hiến, Khuê Mỹ, Ngũ Hành Sơn, Da Nang, Vietnam",
     },
     {
       location:
-        'Trường Đại học Kiến trúc Đà Nẵng, Núi Thành, Hòa Cường, Hòa Cường Nam, Hải Châu District, Da Nang, Vietnam',
+        "Trường Đại học Kiến trúc Đà Nẵng, Núi Thành, Hòa Cường, Hòa Cường Nam, Hải Châu District, Da Nang, Vietnam",
     },
     {
       location:
-        'Bệnh Viện Quân Y C17, Nguyễn Hữu Thọ, Hòa Thuận Tây, Hải Châu District, Da Nang, Vietnam',
+        "Bệnh Viện Quân Y C17, Nguyễn Hữu Thọ, Hòa Thuận Tây, Hải Châu District, Da Nang, Vietnam",
     },
-  ]
+  ];
+
+  // Call api history
+  useEffect(() => {
+    const fetchHistories = async () => {
+      const data = await getHistory();
+      setHistories(data);
+    };
+
+    fetchHistories();
+  }, []);
+
+  // Call api realtime
+  useEffect(() => {
+    // TODO: try catch error
+    const fetchRealtime = async () => {
+      const data = await getRealtime();
+      setRealtime(data);
+    };
+
+    fetchRealtime();
+  }, []);
+
+  if (!isLoaded) {
+    return <SkeletonText />;
+  }
 
   // Calculate route
   const handleCalculateAndDisplayRoute = async () => {
     if (originRef.current === null || destiantionRef.current === null) {
-      return
+      return;
     }
 
+    // Selected multiple bus stops
+    const waypt = [];
+    busStops.forEach((busStop) => {
+      waypt.push({
+        location: busStop.location,
+        stopover: true,
+      });
+    });
+
     // eslint-disable-next-line no-undef
-    const directionsService = new google.maps.DirectionsService()
+    const directionsService = new google.maps.DirectionsService();
     const results = await directionsService.route({
-      origin: originRef.current.value, // Start point
+      origin: currentLocation, // Start point
       waypoints: busStops,
       destination: destiantionRef.current.value, // End point
       // eslint-disable-next-line no-undef
       travelMode: google.maps.TravelMode.DRIVING,
-    })
-    console.log('myResult', results)
-    setDirectionsResponse(results)
-    setDistance(results.routes[0].legs[0].distance.text)
-    setDuration(results.routes[0].legs[0].duration?.text)
-  }
+    });
+    console.log("myResult", results);
+    setDirectionsResponse(results);
+    setDistance(results.routes[0].legs[0].distance.text);
+    setDuration(results.routes[0].legs[0].duration?.text);
+  };
 
-  function clearRoute() {
-    setDirectionsResponse(null)
-    setDistance('')
-    setDuration('')
-    originRef.current.value === ''
-    destiantionRef.current.value === ''
-  }
+  const handleClearRoute = () => {
+    setDirectionsResponse(null);
+    setDistance("");
+    setDuration("");
+    originRef.current.value = "";
+    destiantionRef.current.value = "";
+  };
+
+  const currentLocation = {
+    lat: realtime.result[0].lat,
+    lng: realtime.result[0].lng,
+  };
 
   return (
     <Flex
@@ -124,8 +138,8 @@ function App() {
         {/* Google Map Box */}
         <GoogleMap
           center={center}
-          zoom={20}
-          mapContainerStyle={{ width: '100%', height: '100%' }}
+          zoom={16}
+          mapContainerStyle={{ width: "100%", height: "100%" }}
           options={{
             zoomControl: false,
             streetViewControl: false,
@@ -137,7 +151,7 @@ function App() {
           <Marker position={center} />
           {directionsResponse && (
             <DirectionsRenderer directions={directionsResponse} />
-           )} 
+          )}
         </GoogleMap>
       </Box>
       <Box
@@ -176,8 +190,8 @@ function App() {
                 <option key={index} value={busStop.location}>
                     {busStop.location}
                   </option>
-                ))} */}
-              {/* </Select> */}
+                ))} 
+               </Select> */}
             </Autocomplete>
           </Box>
 
@@ -192,26 +206,27 @@ function App() {
             <IconButton
               aria-label="center back"
               icon={<FaTimes />}
-              onClick={clearRoute}
+              onClick={handleClearRoute}
             />
           </ButtonGroup>
         </HStack>
         <HStack spacing={4} mt={4} justifyContent="space-between">
           <Text>Distance: {distance} </Text>
           <Text>Duration: {duration} </Text>
-          <IconButton
+          <IconButton // Marker current location
             aria-label="center back"
             icon={<FaLocationArrow />}
             isRound
             onClick={() => {
-              map.panTo(center)
-              map.setZoom(20)
+              map.panTo(currentLocation);
+              // map.p
+              map.setZoom(16);
             }}
           />
         </HStack>
       </Box>
     </Flex>
-  )
+  );
 }
 
-export default App
+export default App;
